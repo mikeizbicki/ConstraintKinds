1e6a3dac141bec63f43d95d175bf4bb9927c3a8d
Mike Izbicki <mike@izbicki.me>
2013-06-03 20:11:55 -0700

    Fixed cabal file

--- src/Control/ConstraintKinds.hs
+++ src/Control/ConstraintKinds.hs
@@ -3,8 +3,9 @@ module Control.ConstraintKinds
     , module Control.ConstraintKinds.Filterable
     , module Control.ConstraintKinds.Foldable
     , module Control.ConstraintKinds.Functor
+    , module Control.ConstraintKinds.Monad
     , module Control.ConstraintKinds.Partitionable
---     , module Control.ConstraintKinds.Monad
+    , module Control.ConstraintKinds.Pointed
     , module Control.ConstraintKinds.Traversable
     )
     where
@@ -13,6 +14,7 @@ import Control.ConstraintKinds.Applicative
 import Control.ConstraintKinds.Filterable
 import Control.ConstraintKinds.Foldable
 import Control.ConstraintKinds.Functor
--- import Control.ConstraintKinds.Monad
+import Control.ConstraintKinds.Monad
 import Control.ConstraintKinds.Partitionable
+import Control.ConstraintKinds.Pointed
 import Control.ConstraintKinds.Traversable
